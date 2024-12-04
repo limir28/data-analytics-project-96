06ab94839b7e58e1c166ab55a891ae4ffe7dd879
limir28 <Lizamiron28@gmail.com>
2024-12-04 23:33:00 +0300

    dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -3,5 +3,9 @@ SELECT count(DISTINCT visitor_id) AS visitors_count
 FROM sessions;
 
 -- Кол-во лидов
-SELECT count(DISTINCT lead_id) AS leads_count
+SELECT count(lead_id) AS leads_count
 FROM leads;
+
+--Суммарная выручка
+SELECT SUM(amount) AS amount
+FROM sessions;
